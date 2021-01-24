6f4004af6f02e63b4024ca2d5a35803d3de62289
justine brun <just.brun@hotmail.fr>
2021-01-24 15:28:41 +0100

    ajout etudiant-form

---
+++ src/app/etudiant-form/etudiant-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Profil } from '../modele/profil';
+import { EtudiantsService } from '../services/etudiants.service';
+
+@Component({
+  selector: 'app-etudiant-form',
+  templateUrl: './etudiant-form.component.html',
+  styleUrls: ['./etudiant-form.component.css']
+})
+export class EtudiantFormComponent implements OnInit {
+  @Input() etudiant:Profil|undefined;
+  @Input() id:number =-1;
+  etudiantFormat!: Profil;
+
+  constructor(public servEtu:EtudiantsService) { }
+
+  ngOnInit(): void {
+    if (this.etudiant==undefined) {
+      this.etudiant = {
+        _id:this.id,
+        nom:'',
+        prenom:'',
+        age:0,
+        mail:'',
+        specialites:[''],
+        photo:''
+      }
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.formatFormValue(form);
+      this.servEtu.addStudent(this.etudiantFormat, this.id);
+    }
+  }
+
+  formatFormValue(form: NgForm) {
+    this.etudiantFormat = {
+      nom:form.value.nom,
+      prenom:form.value.prenom,
+      age:form.value.age,
+      mail:form.value.mail,
+      specialites:[form.value.specialites],
+      photo:form.value.photo
+    }
+  }
+}
